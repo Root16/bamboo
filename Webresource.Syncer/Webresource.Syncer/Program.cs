6d5a3846abe6c83af7c8dedd289f199eb6253de9
John Yenter-Briars <jyenterbriars@root16.com>
2022-05-14 10:28:43 -0500

    refactor command line argument shape

--- Webresource.Syncer/Webresource.Syncer/Program.cs
+++ Webresource.Syncer/Webresource.Syncer/Program.cs
@@ -7,25 +7,72 @@ using CommandLine;
 using WebResource.Syncer.SyncLogic;
 using Microsoft.Extensions.Logging.Console;
 using WebResource.Syncer;
+using System.Linq;
+using System.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-await Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsedAsync(async options =>
+var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The file path for the given action")
 {
-    IConfiguration config = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", false)
-        .AddJsonFile("appsettings.Development.json", true)
-        .Build();
+    IsRequired = true,
 
-    if (options.ListWebResources)
-    {
-        var lister = new Lister(config, options);
+};
 
-        await lister.ListFilesInSolutionAsync();
+var solutionOption = new Option<string?>(
+    name: "--solution",
+    description: "Logical name of solution for the given action")
+{
+    IsRequired = true,
+};
 
-        return;
-    }
+var updateIfExistsOption = new Option<bool>(
+    name: "--update-if-exists",
+    getDefaultValue: () => false,
+    description: "If the file already exists in the solution, update it's content");
 
-    IUploader uploader = new Uploader(config, options);
+var rootCommand = new RootCommand("Webresource.Syncer");
 
-    await uploader.UploadFileAsync();
+var uploadCommand = new Command("upload", "Upload the file to PowerApps")
+            {
+                fileOption,
+                solutionOption,
+                updateIfExistsOption,
+            };
 
-});
+var listCommand = new Command("list", "List the Webresources in a given solution")
+{
+    solutionOption,
+};
+
+var publishCommand = new Command("publish", "Publish Webresource in PowerApps")
+{
+    fileOption,
+};
+
+uploadCommand.SetHandler(async (FileInfo fileInfo, string solutionName, bool updateIfExists) =>
+{
+    Console.WriteLine(fileInfo);
+    Console.WriteLine(solutionName);
+    Console.WriteLine(updateIfExists);
+}, fileOption, solutionOption, updateIfExistsOption);
+
+listCommand.SetHandler(async (string solutionName) =>
+{
+    Console.WriteLine(solutionName);
+}, solutionOption);
+
+
+publishCommand.SetHandler(async (FileInfo fileInfo) =>
+{
+    Console.WriteLine(fileInfo);
+}, fileOption);
+
+rootCommand.AddCommand(uploadCommand);
+rootCommand.AddCommand(listCommand);
+rootCommand.AddCommand(publishCommand);
+
+return rootCommand.InvokeAsync(args).Result;
+
